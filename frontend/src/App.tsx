771bf3679b869aeebe07d22b8f004b1321969798
kauanAg-devs <kauanbarcelos77@gmail.com>
2024-06-11 21:53:32 -0300

    feat: add filter-search bar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 import Navigation from './components/Navigation/Navigation';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
      <Header/>
      <Navigation/>
+     <Main/>
     </div>
   );
 }
